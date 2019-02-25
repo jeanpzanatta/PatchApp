11a67256e6333d48abdcc3f5abdd31dadb4a53a9
Jean Pedro Zanatta <jeanpedrozanatta@gmail.com>
2019-02-24 22:22:00 -0400

    configurando para o heroku

--- PatchApp/settings.py
+++ PatchApp/settings.py
@@ -123,7 +123,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'staticfiles'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 LOGIN_URL = '/contas/login'
