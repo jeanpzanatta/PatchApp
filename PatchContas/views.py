eca84c3c771fe79a0d98dbce1d461688b18fda6d
Jean Pedro Zanatta <jeanpedrozanatta@gmail.com>
2019-02-26 00:50:55 -0400

    Adicionado campos de busca

--- PatchContas/views.py
+++ PatchContas/views.py
@@ -11,13 +11,45 @@ def tela_inicial(request):
 
 @login_required
 def compras_lista(request):
-    compras = Compras.objects.all()
+    pesquisa = request.GET.get('pesquisa', None)
+    data_min = request.GET.get('data_min', None)
+    if pesquisa or data_min:
+        compras = Compras.objects.filter(nome__contains=pesquisa) | Compras.objects.filter(descricao__contains=pesquisa)
+        if data_min:
+            compras = compras.filter(data__lte=str(data_min)).order_by('data')
+    else:
+        compras = Compras.objects.all()
     return render(request, 'lista_compras.html', {'compras': compras})
 
 
 @login_required
 def vendas_lista(request):
-    vendas = Vendas.objects.all()
+    pesquisa = request.GET.get('pesquisa', None)
+    radio = request.GET.get('radio_pgto', None)
+    if radio == 'aberto':
+        vendas = Vendas.objects.filter(parcela_um_paga=0) | Vendas.objects.filter(parcela_dois_paga=0)
+        vendas = vendas | Vendas.objects.filter(parcela_tres_paga=0) | Vendas.objects.filter(parcela_quatro_paga=0)
+    elif radio == 'pago':  # TEM QUE OTIMIZAR ESSA BUSCA
+        vendas = Vendas.objects.filter(parcela_um_paga=1,
+                                       parcela_dois_paga=1,
+                                       parcela_tres_paga=1,
+                                       parcela_quatro_paga=1)
+        vendas = vendas | Vendas.objects.filter(parcela_um_paga=1,
+                                                parcela_dois_paga=1,
+                                                parcela_tres_paga=1,
+                                                parcela_quatro_paga=None)
+        vendas = vendas | Vendas.objects.filter(parcela_um_paga=1,
+                                                parcela_dois_paga=1,
+                                                parcela_tres_paga=None,
+                                                parcela_quatro_paga=None)
+        vendas = vendas | Vendas.objects.filter(parcela_um_paga=1,
+                                                parcela_dois_paga=None,
+                                                parcela_tres_paga=None,
+                                                parcela_quatro_paga=None)
+    else:
+        vendas = Vendas.objects.all()
+    if pesquisa:
+        vendas = vendas.filter(nome__contains=pesquisa) | vendas.filter(descricao__contains=pesquisa)
     return render(request, 'lista_vendas.html', {'vendas': vendas})
 
 
